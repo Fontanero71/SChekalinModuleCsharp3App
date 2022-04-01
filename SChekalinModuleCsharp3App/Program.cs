c6fde85bb5d92b045554e803e80ff3c4b2057074
Сергей Чекалин <schekalin@mail.ru>
2022-04-01 17:03:06 +0300

    Add project files.

---
+++ SChekalinModuleCsharp3App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SChekalinModuleCsharp3App
+{
+    class Prorgamm
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Sergei";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
